0e310dd81395dd1e65f50c1bce1397fb2abfdb24
Your Name <you@example.com>
2021-11-10 08:20:53 -0600

    Object Oriented Programming Exercise

---
+++ app.js
@@ -0,0 +1,45 @@
+
+
+// 1a
+const mulan = {
+    title: `Mulan`,
+    main: `Fa Mulan`,
+
+// 1b
+quote: () => `Dishonor! Dishonor on your whole family!`
+
+// 1c
+
+storyline: () => {
+    return `The movie ${this.title} is about ${this.main}`;
+}
+};
+
+
+console.log(mulan.title);
+console.log(mulan.storyline);
+console.log(mulan.quote);
+
+// 2a
+const tangled = {
+    title: `Tangled`,
+    main: `Rapunzel`,
+}
+
+// 2b
+quote: () => `I can't believe I did this! I can't believe I did this! I have to go home! I am never going back! I'm a horrible daughter! BEST DAY EVER!`;
+
+// 2c
+storyline: () => {
+    `The movie ${this.title} is about ${Rapunzel}`};
+
+// 3a
+function disneyMovie(t, m{
+    this.title = t;
+    this.main = m;
+})
+
+// 3b
+disneyMovie(){
+    
+}
